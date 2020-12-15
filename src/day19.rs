80b036510dff62047e895c486fc41f1908d6a6f9
Jordan McMichael <jlmcmchl@gmail.com>
2020-12-15 16:31:54 -0500

    setup for remaining days

---
+++ src/day19.rs
@@ -0,0 +1,40 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day19)]
+pub fn input_generator(_: &str) -> () {
+}
+
+#[aoc(day19, part1)]
+pub fn solve_p1(_: &()) -> usize {
+    0
+}
+
+#[aoc(day19, part2)]
+pub fn solve_p2(_: &()) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn p1_wrapper(input: &str) -> usize {
+        let parsed = input_generator(input);
+        solve_p1(&parsed)
+    }
+
+    fn p2_wrapper(input: &str) -> usize {
+        let parsed = input_generator(input);
+        solve_p2(&parsed)
+    }
+
+    #[test]
+    fn test_p1() {
+        assert!(true);
+    }
+
+    #[test]
+    fn test_p2() {
+        assert!(true);
+    }
+}
