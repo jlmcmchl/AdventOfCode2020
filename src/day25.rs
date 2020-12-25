d724631373c6c9fda883daeb772331f52bff3f33
jlmcmchl <jlmcmchl@gmail.com>
2020-12-25 00:21:13 -0500

    Day 25

--- src/day25.rs
+++ src/day25.rs
@@ -1,16 +1,38 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day25)]
-pub fn input_generator(_: &str) -> () {
+pub fn input_generator(input: &str) -> (usize, usize) {
+    let input = input
+        .lines()
+        .map(|i| i.parse().unwrap())
+        .collect::<Vec<_>>();
+    (input[0], input[1])
 }
 
 #[aoc(day25, part1)]
-pub fn solve_p1(_: &()) -> usize {
-    0
+pub fn solve_p1((card_key, door_key): &(usize, usize)) -> usize {
+    // let card_loops = solve_for_loops(7, *card_key);
+    let door_loops = solve_for_loops(7, *door_key);
+    // println!("{} {}", card_loops, door_loops);
+    // let enc_key = transform(*card_key, door_loops);
+    // assert_eq!(enc_key, transform(*card_key, door_loops));
+    // enc_key
+    transform(*card_key, door_loops)
 }
 
-#[aoc(day25, part2)]
-pub fn solve_p2(_: &()) -> usize {
+fn transform(subject: usize, loops: usize) -> usize {
+    (0..loops).fold(1, |val, _| val * subject % 20201227)
+}
+
+fn solve_for_loops(subject: usize, public: usize) -> usize {
+    let mut current = 1;
+    for i in 0.. {
+        current *= subject;
+        current %= 20201227;
+        if current == public {
+            return i + 1;
+        }
+    }
     0
 }
 
@@ -18,23 +40,12 @@ pub fn solve_p2(_: &()) -> usize {
 mod test {
     use super::*;
 
-    fn p1_wrapper(input: &str) -> usize {
-        let parsed = input_generator(input);
-        solve_p1(&parsed)
-    }
-
-    fn p2_wrapper(input: &str) -> usize {
-        let parsed = input_generator(input);
-        solve_p2(&parsed)
-    }
-
     #[test]
     fn test_p1() {
-        assert!(true);
-    }
-
-    #[test]
-    fn test_p2() {
-        assert!(true);
+        assert_eq!(solve_for_loops(7, 5764801), 8);
+        assert_eq!(solve_for_loops(7, 17807724), 11);
+        assert_eq!(transform(5764801, 11), 14897079);
+        assert_eq!(transform(17807724, 8), 14897079);
+        assert_eq!(solve_p1(&(5764801, 17807724)), 14897079);
     }
 }
